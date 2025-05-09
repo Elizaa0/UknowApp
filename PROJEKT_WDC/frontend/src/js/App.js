12837190c48fb6367da239afac70300ab36f047c
iggy21 <108935584+iggy21@users.noreply.github.com>
2025-05-09 10:12:39 +0200

    Update App.js

--- PROJEKT_WDC/frontend/src/js/App.js
+++ PROJEKT_WDC/frontend/src/js/App.js
@@ -10,7 +10,6 @@ import SharedFlashcards from './SharedFlashcards';
 import TwoFactorAuth from './TwoFactorAuth';
 import TwoFactorSetup from './TwoFactorSetup';
 
-
 function App() {
   return (
     <ErrorBoundary>
